351765cbf4c8af77d12cb7d02342d5d2df89aaed
Matheus Nicolas <manijrprof@gmail.com>
2021-12-13 21:42:48 -0300

    Segundo commit de C# terceira e quarta aula

---
+++ 05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,45 @@
+﻿//using _05_ByteBank;
+
+namespace _05_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDistino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            contaDistino.Depositar(valor);
+            return true;
+
+        }
+    }
+}
+
+
+
---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissão = "Desenvolvedora C#";
+            //gabriela.cpf = "434.562.878-20";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = gabriela;
+            //conta.titular = new Cliente();
+
+            //conta.titular.nome = "Gabriela costa";
+            //conta.titular.cpf = "434.562.878-20";
+            //conta.titular.profissão = "Desenvolvedora C#";
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            if (conta.titular == null)
+            {
+                Console.WriteLine("Ops, a referênvia em conta.titular e NULL");
+            }
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular);
+            //Console.WriteLine(conta.titular.nome);
+            //Console.WriteLine(conta.titular.cpf);
+            //Console.WriteLine(conta.titular.profissão);
+
+        }
+    }
+}
